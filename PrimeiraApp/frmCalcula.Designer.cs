8cffb2b7eda971192a3853c8dfeb176983d9b6a9
bruno.jogomes <bruno.jogomes@senacsp.edu.br>
2022-12-08 15:29:38 -0300

    Clone app prof

---
+++ PrimeiraApp/frmCalcula.Designer.cs
@@ -0,0 +1,246 @@
+﻿
+namespace PrimeiraApp
+{
+    partial class frmCalcula
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmCalcula));
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.lblNum1 = new System.Windows.Forms.Label();
+            this.lblNum2 = new System.Windows.Forms.Label();
+            this.txtNum1 = new System.Windows.Forms.TextBox();
+            this.txtNum2 = new System.Windows.Forms.TextBox();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.lblResultadoSoma = new System.Windows.Forms.Label();
+            this.btnSoma = new System.Windows.Forms.Button();
+            this.btnSubtracao = new System.Windows.Forms.Button();
+            this.btnMulti = new System.Windows.Forms.Button();
+            this.btnDivisao = new System.Windows.Forms.Button();
+            this.lblRespostaSoma = new System.Windows.Forms.Label();
+            this.lblRespostaSub = new System.Windows.Forms.Label();
+            this.lblResultadoSub = new System.Windows.Forms.Label();
+            this.lblRespostaMult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(78, 34);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(192, 29);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "Calcula valores";
+            // 
+            // lblNum1
+            // 
+            this.lblNum1.AutoSize = true;
+            this.lblNum1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNum1.Location = new System.Drawing.Point(47, 122);
+            this.lblNum1.Name = "lblNum1";
+            this.lblNum1.Size = new System.Drawing.Size(82, 20);
+            this.lblNum1.TabIndex = 1;
+            this.lblNum1.Text = "Número 1:\r\n";
+            // 
+            // lblNum2
+            // 
+            this.lblNum2.AutoSize = true;
+            this.lblNum2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNum2.Location = new System.Drawing.Point(47, 218);
+            this.lblNum2.Name = "lblNum2";
+            this.lblNum2.Size = new System.Drawing.Size(82, 20);
+            this.lblNum2.TabIndex = 2;
+            this.lblNum2.Text = "Número 2:";
+            // 
+            // txtNum1
+            // 
+            this.txtNum1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNum1.Location = new System.Drawing.Point(136, 122);
+            this.txtNum1.MaxLength = 20;
+            this.txtNum1.Name = "txtNum1";
+            this.txtNum1.Size = new System.Drawing.Size(158, 26);
+            this.txtNum1.TabIndex = 0;
+            // 
+            // txtNum2
+            // 
+            this.txtNum2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNum2.Location = new System.Drawing.Point(136, 210);
+            this.txtNum2.MaxLength = 20;
+            this.txtNum2.Name = "txtNum2";
+            this.txtNum2.Size = new System.Drawing.Size(158, 26);
+            this.txtNum2.TabIndex = 1;
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpar.Location = new System.Drawing.Point(191, 338);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(103, 40);
+            this.btnLimpar.TabIndex = 3;
+            this.btnLimpar.Text = "&Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            // 
+            // lblResultadoSoma
+            // 
+            this.lblResultadoSoma.AutoSize = true;
+            this.lblResultadoSoma.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultadoSoma.Location = new System.Drawing.Point(127, 442);
+            this.lblResultadoSoma.Name = "lblResultadoSoma";
+            this.lblResultadoSoma.Size = new System.Drawing.Size(0, 20);
+            this.lblResultadoSoma.TabIndex = 8;
+            // 
+            // btnSoma
+            // 
+            this.btnSoma.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSoma.Location = new System.Drawing.Point(24, 329);
+            this.btnSoma.Name = "btnSoma";
+            this.btnSoma.Size = new System.Drawing.Size(49, 26);
+            this.btnSoma.TabIndex = 9;
+            this.btnSoma.Text = "+";
+            this.btnSoma.UseVisualStyleBackColor = true;
+            this.btnSoma.Click += new System.EventHandler(this.btnSoma_Click);
+            // 
+            // btnSubtracao
+            // 
+            this.btnSubtracao.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSubtracao.Location = new System.Drawing.Point(80, 329);
+            this.btnSubtracao.Name = "btnSubtracao";
+            this.btnSubtracao.Size = new System.Drawing.Size(49, 26);
+            this.btnSubtracao.TabIndex = 10;
+            this.btnSubtracao.Text = "-";
+            this.btnSubtracao.UseVisualStyleBackColor = true;
+            this.btnSubtracao.Click += new System.EventHandler(this.btnSubtracao_Click);
+            // 
+            // btnMulti
+            // 
+            this.btnMulti.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnMulti.Location = new System.Drawing.Point(24, 366);
+            this.btnMulti.Name = "btnMulti";
+            this.btnMulti.Size = new System.Drawing.Size(49, 26);
+            this.btnMulti.TabIndex = 11;
+            this.btnMulti.Text = "x";
+            this.btnMulti.UseVisualStyleBackColor = true;
+            this.btnMulti.Click += new System.EventHandler(this.btnMulti_Click);
+            // 
+            // btnDivisao
+            // 
+            this.btnDivisao.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDivisao.Location = new System.Drawing.Point(82, 366);
+            this.btnDivisao.Name = "btnDivisao";
+            this.btnDivisao.Size = new System.Drawing.Size(47, 26);
+            this.btnDivisao.TabIndex = 12;
+            this.btnDivisao.Text = "/";
+            this.btnDivisao.UseVisualStyleBackColor = true;
+            this.btnDivisao.Click += new System.EventHandler(this.btnDivisao_Click);
+            // 
+            // lblRespostaSoma
+            // 
+            this.lblRespostaSoma.AutoSize = true;
+            this.lblRespostaSoma.Location = new System.Drawing.Point(38, 449);
+            this.lblRespostaSoma.Name = "lblRespostaSoma";
+            this.lblRespostaSoma.Size = new System.Drawing.Size(83, 13);
+            this.lblRespostaSoma.TabIndex = 13;
+            this.lblRespostaSoma.Text = "Resposta soma:";
+            // 
+            // lblRespostaSub
+            // 
+            this.lblRespostaSub.AutoSize = true;
+            this.lblRespostaSub.Location = new System.Drawing.Point(38, 476);
+            this.lblRespostaSub.Name = "lblRespostaSub";
+            this.lblRespostaSub.Size = new System.Drawing.Size(105, 13);
+            this.lblRespostaSub.TabIndex = 14;
+            this.lblRespostaSub.Text = "Resposta subtração:";
+            // 
+            // lblResultadoSub
+            // 
+            this.lblResultadoSub.AutoSize = true;
+            this.lblResultadoSub.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultadoSub.Location = new System.Drawing.Point(149, 469);
+            this.lblResultadoSub.Name = "lblResultadoSub";
+            this.lblResultadoSub.Size = new System.Drawing.Size(0, 20);
+            this.lblResultadoSub.TabIndex = 15;
+            // 
+            // lblRespostaMult
+            // 
+            this.lblRespostaMult.AutoSize = true;
+            this.lblRespostaMult.Location = new System.Drawing.Point(38, 503);
+            this.lblRespostaMult.Name = "lblRespostaMult";
+            this.lblRespostaMult.Size = new System.Drawing.Size(119, 13);
+            this.lblRespostaMult.TabIndex = 16;
+            this.lblRespostaMult.Text = "Resposta multiplicação:";
+            // 
+            // frmCalcula
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(333, 613);
+            this.Controls.Add(this.lblRespostaMult);
+            this.Controls.Add(this.lblResultadoSub);
+            this.Controls.Add(this.lblRespostaSub);
+            this.Controls.Add(this.lblRespostaSoma);
+            this.Controls.Add(this.btnDivisao);
+            this.Controls.Add(this.btnMulti);
+            this.Controls.Add(this.btnSubtracao);
+            this.Controls.Add(this.btnSoma);
+            this.Controls.Add(this.lblResultadoSoma);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.txtNum2);
+            this.Controls.Add(this.txtNum1);
+            this.Controls.Add(this.lblNum2);
+            this.Controls.Add(this.lblNum1);
+            this.Controls.Add(this.lblTitulo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "frmCalcula";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculadora";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Label lblNum1;
+        private System.Windows.Forms.Label lblNum2;
+        private System.Windows.Forms.TextBox txtNum1;
+        private System.Windows.Forms.TextBox txtNum2;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Label lblResultadoSoma;
+        private System.Windows.Forms.Button btnSoma;
+        private System.Windows.Forms.Button btnSubtracao;
+        private System.Windows.Forms.Button btnMulti;
+        private System.Windows.Forms.Button btnDivisao;
+        private System.Windows.Forms.Label lblRespostaSoma;
+        private System.Windows.Forms.Label lblRespostaSub;
+        private System.Windows.Forms.Label lblResultadoSub;
+        private System.Windows.Forms.Label lblRespostaMult;
+    }
+}
---
+++ PrimeiraApp/frmCalcula.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PrimeiraApp
+{
+    public partial class frmCalcula : Form
+    {
+        public frmCalcula()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSoma_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSubtracao_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnMulti_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDivisao_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
